3a668fddcbc49d4740525a57afc0f578a3ec2aa7
saikiranbali <balisaikiran@gmail.com>
2021-11-27 20:26:40 +0530

    Add all my files

--- practice/string_palndrome.py
+++ practice/string_palndrome.py
@@ -1,3 +1,4 @@
+# solution 1
 st = input("enter a string : ") #saikiran
 ts =""
 val = range(len(st)-1,-1,-1)
@@ -8,3 +9,30 @@ if(st == ts):
     print("Palendrome")
 else:
     print("not a palin")
+
+
+# solution 2
+wrd=input("Please enter a word")
+wrd=str(wrd)
+rvs=wrd[::-1]
+print(rvs)
+if wrd == rvs:
+    print("This word is a palindrome")
+else:
+    print("This word is not a palindrome")
+
+
+
+# solution 3
+def reverse(word):
+	x = ''
+	for i in range(len(word)):
+		x += word[len(word)-1-i]
+	return x
+
+word = input('give me a word:\n')
+x = reverse(word)
+if x == word:
+	print('This is a Palindrome')
+else:
+	print('This is NOT a Palindrome')
