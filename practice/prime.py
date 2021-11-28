37f7a6b1cffa009da12d85be8bf11bf2ac38d6db
saikiranbali <balisaikiran@gmail.com>
2021-11-28 18:47:13 +0530

    renamed the prime file

--- practice/prime.py
+++ practice/prime.py
@@ -13,3 +13,19 @@ def prime(val):
 
 a = int(input("Enter a nunber : "))
 print(prime(a))
+
+#solution 2 using list comprehessions
+
+num = int(input('Insert a number: '))
+a = [x for x in range(2, num) if num % x == 0]
+
+def is_prime(n):
+	if num > 1:
+		if len(a) == 0:
+			print("Prime")
+		else:
+			print ("NOT prime")
+	else:
+		print ("NOT prime")
+	
+is_prime(num)
