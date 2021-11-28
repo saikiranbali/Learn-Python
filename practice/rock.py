2a5715eff43af57ce8b70476d500559ff7a9530b
saikiranbali <balisaikiran@gmail.com>
2021-11-28 16:47:17 +0530

    updated with 3 solutions

--- practice/rock.py
+++ practice/rock.py
@@ -8,6 +8,7 @@
 # Paper beats rock
 # Scissors beats paper
 
+#Solution 1
 
 p1 = input("P1 : Enter your name : ")
 p2 = input("P2 : Enter your name : ")
@@ -37,3 +38,41 @@ while dec != 'No':
         else:
             print(p1+" Wins")
         dec=input("Play Again (Yes or No) : ")
+    else:
+        print("\nEnter a value b/w 1-3")
+        dec="Yes"
+
+
+#Solution 2
+import sys
+
+user1 = input("What's your name?")
+user2 = input("And your name?")
+user1_answer = input("%s, do yo want to choose rock, paper or scissors?" % user1)
+user2_answer = input("%s, do you want to choose rock, paper or scissors?" % user2)
+
+def compare(u1, u2):
+    if u1 == u2:
+        return("It's a tie!")
+    elif u1 == 'rock':
+        if u2 == 'scissors':
+            return("Rock wins!")
+        else:
+            return("Paper wins!")
+    elif u1 == 'scissors':
+        if u2 == 'paper':
+            return("Scissors win!")
+        else:
+            return("Rock wins!")
+    elif u1 == 'paper':
+        if u2 == 'rock':
+            return("Paper wins!")
+        else:
+            return("Scissors win!")
+    else:
+        return("Invalid input! You have not entered rock, paper or scissors, try again.")
+        sys.exit()
+
+print(compare(user1_answer, user2_answer))
+
+#Clever solution 3
