69371da1d1ea777f8fa3cb7e18d66b916761cafc
saikiranbali <balisaikiran@gmail.com>
2021-11-28 17:24:24 +0530

    added solution 2

--- practice/random.py
+++ practice/random.py
@@ -26,3 +26,30 @@ while guess != 'exit':
         dec +=1
     
 print("Guess after "+str(dec)+" attempts")
+
+
+
+#Solution 2
+
+import random
+
+rd = random.randint(1,9)
+guess = 0
+c = 0
+while guess != rd and guess != "exit":
+    guess = input("Enter a guess between 1 to 9")
+
+    if guess == "exit":
+        break
+
+    guess = int(guess)
+    c += 1
+
+    if guess < rd:
+        print("Too low")
+    elif guess > rd:
+        print("Too high")
+    else:
+        print("Right!")
+        print("You took only", c, "tries!")
+input()
